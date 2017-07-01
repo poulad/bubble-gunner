2b91f6d6161e00b466708f78c8626a16658bcfb1
Poulad <poulad.pld@gmail.com>
2017-06-30 20:49:08 -0400

    Tween circle for test

--- docs/assets/scripts/script.js
+++ docs/assets/scripts/script.js
@@ -1,6 +1,48 @@
 var Script;
 (function (Script) {
+    var Stage = createjs.Stage;
+    var Shape = createjs.Shape;
+    var Ticker = createjs.Ticker;
+    var Tween = createjs.Tween;
+    var Ease = createjs.Ease;
     var canvas;
+    var stage;
+    var bg;
+    var circle;
+    function beginMoveCircle(ev) {
+        function setBgToDark() {
+            bg.graphics
+                .clear()
+                .beginFill("#555")
+                .drawRect(0, 0, canvas.width, canvas.height);
+        }
+        bg.graphics
+            .clear()
+            .beginFill("#458")
+            .drawRect(0, 0, canvas.width, canvas.height);
+        Tween.get(circle)
+            .to({
+            x: ev.stageX,
+            y: ev.stageY
+        }, undefined, Ease.backInOut)
+            .call(setBgToDark);
+    }
+    function draw() {
+        bg = new Shape();
+        bg.graphics
+            .beginFill('#555')
+            .drawRect(0, 0, canvas.width, canvas.height);
+        stage.addChild(bg);
+        circle = new Shape();
+        circle.graphics
+            .beginFill("red")
+            .beginStroke('yellow')
+            .drawCircle(0, 0, 20);
+        circle.x = canvas.width / 2;
+        circle.y = canvas.height / 2;
+        stage.addChild(circle);
+        circle.addEventListener("pressmove", beginMoveCircle);
+    }
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -9,6 +51,10 @@ var Script;
         canvas = document.getElementById("canvas");
         window.addEventListener("resize", resizeCanvas, false);
         resizeCanvas();
+        stage = new Stage(canvas);
+        Ticker.framerate = 60;
+        Ticker.addEventListener("tick", function () { return stage.update(); });
+        draw();
     }
     Script.init = init;
 })(Script || (Script = {}));
--- docs/assets/scripts/script.ts
+++ docs/assets/scripts/script.ts
@@ -1,5 +1,58 @@
 namespace Script {
+    import MouseEvent = createjs.MouseEvent;
+    import Stage = createjs.Stage;
+    import Shape = createjs.Shape;
+    import Ticker = createjs.Ticker;
+    import Tween = createjs.Tween;
+    import Ease = createjs.Ease;
+
     let canvas: HTMLCanvasElement;
+    let stage: Stage;
+    let bg: Shape;
+    let circle: Shape;
+
+
+    function beginMoveCircle(ev: MouseEvent) {
+        function setBgToDark() {
+            bg.graphics
+                .clear()
+                .beginFill(`#555`)
+                .drawRect(0, 0, canvas.width, canvas.height);
+        }
+
+        bg.graphics
+            .clear()
+            .beginFill(`#458`)
+            .drawRect(0, 0, canvas.width, canvas.height);
+
+        Tween.get(circle)
+            .to({
+                x: ev.stageX,
+                y: ev.stageY
+            }, undefined, Ease.backInOut)
+            .call(setBgToDark);
+    }
+
+    function draw() {
+        bg = new Shape();
+        bg.graphics
+            .beginFill('#555')
+            .drawRect(0, 0, canvas.width, canvas.height);
+        stage.addChild(bg);
+
+        circle = new Shape();
+        circle.graphics
+            .beginFill(`red`)
+            .beginStroke('yellow')
+            .drawCircle(0, 0, 20);
+
+        circle.x = canvas.width / 2;
+        circle.y = canvas.height / 2;
+
+        stage.addChild(circle);
+
+        circle.addEventListener(`pressmove`, beginMoveCircle);
+    }
 
     function resizeCanvas() {
         canvas.width = window.innerWidth;
@@ -10,6 +63,12 @@ namespace Script {
         canvas = <HTMLCanvasElement> document.getElementById(`canvas`);
         window.addEventListener(`resize`, resizeCanvas, false);
         resizeCanvas();
+
+        stage = new Stage(canvas);
+        Ticker.framerate = 60;
+        Ticker.addEventListener(`tick`, () => stage.update());
+
+        draw();
     }
 }
 
