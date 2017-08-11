1f4d80eeb671a3896bc681cee1d13ed285d388e3
Poulad <poulad.pld@gmail.com>
2017-08-11 19:03:49 -0400

    Bundle and uglify js files

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        ts: {
+            bubbleGunner: {
+                src: [
+                    "**/*.ts",
+                    "!node_modules/**"
+                ]
+            }
+        },
+        concat: {
+            bubbleGunner: {
+                src: [
+                    "docs/assets/scripts/bubble-gunner2.js",
+                    "docs/assets/scripts/preload.js",
+                    "docs/assets/scripts/menu.js",
+                    "docs/assets/scripts/help.js",
+                    "docs/assets/scripts/game.js",
+                    "docs/assets/scripts/game-over.js",
+                    "docs/assets/scripts/bubble-gunner.js",
+                    "docs/assets/scripts/script.js"
+                ],
+                dest: "docs/assets/scripts/bubble-gunner.concat.js"
+            }
+        },
+        uglify: {
+            bubbleGunner: {
+                src: ["docs/assets/scripts/bubble-gunner.concat.js"],
+                dest: "docs/assets/scripts/bubble-gunner.min.js"
+            }
+        },
+        clean: {
+            bubbleGunner: {
+                src: ["docs/assets/scripts/bubble-gunner.concat.js"]
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask("default", [
+        "ts:bubbleGunner",
+        "concat:bubbleGunner",
+        "uglify:bubbleGunner",
+        "clean:bubbleGunner"
+    ]);
+};
